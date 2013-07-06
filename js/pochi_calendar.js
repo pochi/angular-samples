696604c33f1c22615f52c9e6bc9c0523357ae312
Yuichi Kuroda <pochi.black@gmail.com>
2013-07-07 00:20:09 +0900

    Change Japanese mode

--- js/pochi_calendar.js
+++ js/pochi_calendar.js
@@ -54,6 +54,7 @@ calendarApp.controller('CalendarCtrl', function($scope) {
     $scope.myCalendar.fullCalendar('changeView', view);
   };
 
+  // http://iw3.me/156/
   $scope.uiConfig = {
     calendar:{
       height: 450,
@@ -65,7 +66,34 @@ calendarApp.controller('CalendarCtrl', function($scope) {
       },
       dayClick: $scope.alertEventOnClick,
       eventDrop: $scope.alertOnDrop,
-      eventResize: $scope.alertOnResize
+      eventResize: $scope.alertOnResize,
+      weekends: false,
+      firstDay: 1,
+      // 月名称
+      monthNames: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+      // 月略称
+      monthNamesShort: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+      // 曜日名称
+      dayNames: ['日曜日', '月曜日', '火曜日', '水曜日', '木曜日', '金曜日', '土曜日'],
+      // 曜日略称
+      dayNamesShort: ['日曜日', '月曜日', '火曜日', '水曜日', '木曜日', '金曜日', '土曜日'],
+      // ボタン文字列
+      buttonText: {
+          prev:     '&lsaquo;', // <
+          next:     '&rsaquo;', // >
+          prevYear: '&laquo;',  // <<
+          nextYear: '&raquo;',  // >>
+          today:    '今月',
+          month:    '月',
+          week:     '週',
+          day:      '日'
+      },
+      // タイトルの書式
+      titleFormat: {
+          month: 'yyyy年M月',                             // 2013年9月
+          week: "yyyy年M月d日{ ～ }{[yyyy年]}{[M月]d日}", // 2013年9月7日 ～ 13日
+          day: "yyyy年M月d日'('ddd')'"                  // 2013年9月7日(火)
+      }
     }
   };
 
